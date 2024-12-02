115c08d1ea3e41eae99f44ee8796345ace2c66b8
Jack LaBarba <brakit@gmail.com>
2024-12-01 17:24:41 -0800

    Add 1p AI mode to title screen

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,19 +6,26 @@ public class UIManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void open1PScene(){
+    public void open1PScene()
+    {
         SceneManager.LoadScene(1);
     }
-    public void openPVPScene(){
+
+    public void open1PAIScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void openPVPScene()
+    {
         SceneManager.LoadScene(2);
     }
     // need to finish
